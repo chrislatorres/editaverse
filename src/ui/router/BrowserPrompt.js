541adb046efb2f69372bb0b11dc74f2364941b51
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-26 18:01:46 -0600

    do not use beforeunload event listener

--- src/ui/router/BrowserPrompt.js
+++ src/ui/router/BrowserPrompt.js
@@ -10,7 +10,7 @@ export default class BrowserPrompt extends React.Component {
   constructor(props) {
     super(props);
 
-    window.addEventListener("beforeunload", this.onBeforeUnload);
+//    window.addEventListener("beforeunload", this.onBeforeUnload);
   }
 
   onBeforeUnload = e => {
